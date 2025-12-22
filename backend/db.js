6f49b911d5683449fb59bdca0589516ecec17840
goldyy12 <diarselmani10@gmail.com>
2025-12-22 17:27:17 +0100

    prisma changes

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,19 @@
 // db.js
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import prismaPkg from "@prisma/client";
+import adapterPkg from "@prisma/adapter-pg";
 import dotenv from "dotenv";
 
-
 dotenv.config();
+
+const { PrismaClient } = prismaPkg;
+const { PrismaPg } = adapterPkg;
+
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
 });
 
 const prisma = new PrismaClient({ adapter });
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+console.log("DATABASE_URL loaded:", !!process.env.DATABASE_URL);
 
 export default prisma;
-console.log("DATABASE_URL:", process.env.DATABASE_URL);  
