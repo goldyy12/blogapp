e45ac5f56764455f2b4b9132454c7f15369425d4
goldyy12 <diarselmani10@gmail.com>
2025-12-22 18:14:28 +0100

    prisma change2

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,3 @@
-// db.js
-import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
-
-dotenv.config();
-
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
-console.log("DATABASE_URL loaded:", !!process.env.DATABASE_URL);
-
-export default prisma;
+module.exports = prisma;
